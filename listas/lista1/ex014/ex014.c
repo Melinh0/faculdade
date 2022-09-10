fe3f9cf173d62465482f49f98214f801a6ccdbdc
Rodrigo <teixeirarodrigo2311@gmail.com>
2022-09-10 07:51:29 -0300

    atualização lista 1

---
+++ listas/lista1/ex014/ex014.c
@@ -0,0 +1,12 @@
+// Escreva um programa que leia uma temperatura em graus Celsius e apresente convertida em graus
+// Fahrenheit. A fórmula de versão  ́e F = (9 * C + 160) / 5, sendo F a temperatura Fahrenheit e C a
+// temperatura em Celsius.
+
+#include <stdio.h>
+
+int main(void){
+    float C;
+    float F;
+
+    print("")
+}
